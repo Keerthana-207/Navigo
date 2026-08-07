33ffcb722081eb77b35ec36f9a6cbda49366cec9
Keerthana <keert_207@yahoo.com>
2026-08-07 23:25:08 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/dashboard/Dashboard'
 import PlanTrip from './pages/dashboard/PlanTripPage'
+import BudgetPage from './pages/dashboard/BudgetPage'
+import Budget from './pages/dashboard/Budget'
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Dashboard />} />
       <Route path='/plan-trip' element={<PlanTrip />} />
+      <Route path='/budget' element={<Budget />} />
     </Routes>
     </>
   )
