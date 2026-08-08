cf8c13237976bff49b751dfbb8d6cb2a7bcc61fe
Keerthana <keert_207@yahoo.com>
2026-08-08 20:19:51 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -33,8 +33,10 @@ app.use(
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:5175",
-            "https://navigo-travel.netlify.app",
+            "https://navigo-frontend-livid.vercel.app",
         ],
+         methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     })
 );
