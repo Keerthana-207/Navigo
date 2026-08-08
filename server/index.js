159450d28c7e1091fbbcd9f6d7ef1381ac57ab9c
Keerthana <keert_207@yahoo.com>
2026-08-08 17:24:53 +0530

    update deployment link

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(helmet());
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://navigo-travel.netlify.app/"],
         credentials: true,
     })
 );
